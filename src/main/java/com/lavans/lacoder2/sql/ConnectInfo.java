538a9a2ee7c22db47c815bcb31078fca818a6ed6
dobashi <dobashi@lavans.jp>
2014-02-18 02:21:09 +0900

    lombok化
    Change-Id: I8a261910ab298abb78deb9dcb15d01980fbfba6d

--- src/main/java/com/lavans/lacoder2/sql/ConnectInfo.java
+++ src/main/java/com/lavans/lacoder2/sql/ConnectInfo.java
@@ -1,5 +1,10 @@
 package com.lavans.lacoder2.sql;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class ConnectInfo {
 	private String driverName = null;
 	private String url     = null;
@@ -15,42 +20,4 @@ public class ConnectInfo {
 	 * SQLログを出力するか。
 	 */
 	private boolean isLogging = true;
-
-	public boolean isStatistics() {
-		return isStatistics;
-	}
-	public void setStatistics(boolean isStatistics) {
-		this.isStatistics = isStatistics;
-	}
-	public boolean isLogging() {
-		return isLogging;
-	}
-	public void setLogging(boolean isLogging) {
-		this.isLogging = isLogging;
-	}
-	public String getDriverName() {
-		return driverName;
-	}
-	public void setDriverName(String driverName) {
-		this.driverName = driverName;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getUser() {
-		return user;
-	}
-	public void setUser(String user) {
-		this.user = user;
-	}
-	public String getPass() {
-		return pass;
-	}
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-
 }
